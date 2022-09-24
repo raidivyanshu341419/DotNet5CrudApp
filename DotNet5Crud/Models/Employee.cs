518b1a9f5131ecfba2b3fa2b18926e127337177a
Divyanshu Rai <88521161+raidivyanshu341419@users.noreply.github.com>
2022-09-24 19:27:44 +0530

    Add project files.

---
+++ DotNet5Crud/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace DotNet5Crud.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Designation { get; set; }
+
+        [Required]
+        [Display(Name = "Employee Salary")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [Display(Name = "Joining Date")]
+        public DateTime JoiningDate { get; set; }
+    }
+}
